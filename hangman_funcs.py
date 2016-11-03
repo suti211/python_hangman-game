9fa91f8c3a8e076bd100dc60bfd08bc3dcdfa1c2
Noteee <lugosi.norbert@gmail.com>
2016-11-03 11:09:23 +0100

    pick a word function

--- hangman_funcs.py
+++ hangman_funcs.py
@@ -1,13 +1,7 @@
-#init
-import random
-
-kutyafasz=["valami", "valami2", "valami3", "valami4"]
-
-
 def pick_a_word(x):
     import random
     the_word = (x[random.randrange(len(x))])
     return the_word
 
-print(pick_a_word(kutyafasz))
+
 
