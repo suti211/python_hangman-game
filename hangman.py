b1e6634cd62c9162d5e1c4a2ef581b1a648e6fe8
Noteee <lugosi.norbert@gmail.com>
2016-11-03 11:20:37 +0100

    the functions from hangman.py are copied and saved to hangman_funcs.py

--- hangman.py
+++ hangman.py
@@ -2,22 +2,6 @@
 import hangman_funcs
 
 
-# reads in a file into an array and returns that array.
-# usage : my_List = loadInWords("Filename.txt")
-def loadInWords(FILE):
-    myFile = open(FILE, "r")
-    array = myFile.read().split("\n")
-
-    # removing empty lines from array
-    i = 0
-    while i < len(array):
-        if array[i] == "":
-            del array[i]
-        else:
-            i += 1
-
-    myFile.close()
-    return array
 
 myList = loadInWords("teszt.txt")
 print(myList)
--- hangman_funcs.py
+++ hangman_funcs.py
@@ -1,7 +1,25 @@
+#picks and returns random elment of an "x" list
+#usage : pick_a_word(listname)
 def pick_a_word(x):
     import random
     the_word = (x[random.randrange(len(x))])
     return the_word
 
+# reads in a file into an array and returns that array.
+# usage : my_List = loadInWords("Filename.txt")
+def loadInWords(FILE):
+    myFile = open(FILE, "r")
+    array = myFile.read().split("\n")
+
+    # removing empty lines from array
+    i = 0
+    while i < len(array):
+        if array[i] == "":
+            del array[i]
+        else:
+            i += 1
+
+    myFile.close()
+    return array
 
 
